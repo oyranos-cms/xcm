eef1ae829abf7a16b04a6600f0be7caff70444e0
Kai-Uwe Behrmann <ku.b@gmx.de>
2010-12-29 17:47:17 +0100

    * [core]: add --i2c and --force-output to xcmddc
    Give proper return value for errors during fetching a single i2c device node.
    
    --force-output is a override of non passing EDID header. Still only data
    which is available can be dumped out.
    
    --i2c selects a device node by name. This will speed up the call to xcmddc as
    not all possible device nodes must be scanned to obtain the positional
    node number.

--- src/xcmddc/xcmddc.c
+++ src/xcmddc/xcmddc.c
@@ -33,7 +33,10 @@ void printfHelp(int argc, char ** argv)
   fprintf( stderr, "      %s -l\n",        argv[0]);
   fprintf( stderr, "\n");
   fprintf( stderr, "  %s\n",               _("Dump EDID from i2c:"));
-  fprintf( stderr, "      %s -d number\n",        argv[0]);
+  fprintf( stderr, "      %s -d positional_number [--force-output]\n", argv[0]);
+  fprintf( stderr, "\n");
+  fprintf( stderr, "  %s\n",               _("Dump EDID from i2c:"));
+  fprintf( stderr, "      %s --i2c node_name [--force-output]\n",    argv[0]);
   fprintf( stderr, "\n");
   fprintf( stderr, "  %s\n",               _("Print a help text:"));
   fprintf( stderr, "      %s -h\n",        argv[0]);
@@ -92,16 +95,18 @@ void printfHelp(int argc, char ** argv)
 
 int main(int argc, char ** argv)
 {
-  int error = 0;
+  XCM_DDC_ERROR_e error = 0;
   int i;
   char * data = 0;
   size_t size;
-  char * fn = calloc(sizeof(char),1024);
   int list_devices = 0;
   int device = -1;
+  char ** devices = NULL;
+  int force_output = 0;
   int verbose = 0;
   int n = 0;
-  int pos = 0;
+  const char * i2c_node_name = NULL;
+  int result = 0;
 
 #ifdef USE_GETTEXT
   setlocale(LC_ALL,"");
@@ -124,6 +129,13 @@ int main(int argc, char ** argv)
               case 'v': verbose += 1; break;
               case 'h':
               case '-':
+                        if(i == 1)
+                        {
+                             if(OY_IS_ARG("i2c"))
+                        { OY_PARSE_STRING_ARG2(i2c_node_name, "i2c"); break; }
+                             if(OY_IS_ARG("force-output"))
+                        { force_output = 1; i=100; break; }
+                        }
               default:
                         printfHelp(argc, argv);
                         exit (0);
@@ -149,6 +161,9 @@ int main(int argc, char ** argv)
                         exit (0);
   }
 
+  if(list_devices || !i2c_node_name)
+    error = XcmDDClist( &devices, &n );
+
   if(list_devices)
   {
     if(!verbose)
@@ -159,10 +174,58 @@ int main(int argc, char ** argv)
       fprintf( stderr, "\n");
     } else
     {
+      for(i = 0; i < n; ++i)
+      {
+        puts(devices[i]);
+        fflush( stdout );
+      }
     }
   }
 
-  return 0;
+  if(device != -1 || i2c_node_name)
+  {
+    if(!i2c_node_name &&
+       (device <0 || n <= device))
+    {
+      fprintf( stderr, "device out of range: %d; select >= 0 && < %d\n",
+               device, n );
+    } else
+    {
+      if(!i2c_node_name)
+        i2c_node_name = devices[device];
+      error = XcmDDCgetEDID( i2c_node_name, &data, &size );
+      if(error)
+      {
+        result = error;
+
+        if(verbose)
+          fprintf(stderr, "ERROR[%s]: %s\n", i2c_node_name,
+                                             XcmDDCErrorToString(error) );
+      }
+
+      if(error == XCM_DDC_OK || force_output)
+      {
+        if(verbose)
+          fprintf(stderr, "%s\n", i2c_node_name );
+        if(data)
+        {
+          fwrite( data, sizeof(char), size, stdout );
+          free(data); data = 0; size = 0;
+        }
+        fflush( stdout );
+      }
+    }
+  }
+
+  if(devices && n)
+    for(i = 0; i < n; ++i)
+      if(devices[i])
+        free(devices[i]);
+
+  if(devices)
+    free(devices);
+
+  return result;
 }
 
 
